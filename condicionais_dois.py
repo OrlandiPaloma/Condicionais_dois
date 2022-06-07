9eb3d7812ac683fbff3a6c7cb81c9b5e9267144c
Ubiratan <palomaorlandiw@gmail.com>
2022-06-07 15:18:38 -0300

    condicionaisdois

---
+++ condicionais_dois.py
@@ -0,0 +1,13 @@
+# Condicionais
+# if, elif, else
+'''
+eu cheguei atrasado na aula, ainda posso entrar
+Se essa não for sua terceira vez chegando atrasado então pode sim.
+Caso contrário, ira tomar uma suspensão.
+'''
+
+vez_atrasado = 8
+if vez_atrasado <3:
+  print('pode entrar')
+else:
+  print('esta suspenso')
